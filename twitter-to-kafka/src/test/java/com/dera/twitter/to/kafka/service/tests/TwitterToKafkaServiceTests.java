b1feaa306f881e56b2b10078f182249257aa9848
Chidera Uzoigwe <chiderauzoigwe2@gmail.com>
2024-08-19 16:06:10 +0100

    first service

---
+++ twitter-to-kafka/src/test/java/com/dera/twitter/to/kafka/service/tests/TwitterToKafkaServiceTests.java
@@ -0,0 +1,13 @@
+package com.dera.twitter.to.kafka.service.tests;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class TwitterToKafkaServiceTests {
+
+    @Test
+    public void contextLoad(){
+
+    }
+}
